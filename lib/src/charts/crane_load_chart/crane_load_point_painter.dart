3b54881766906d9292f4d33d8d0188d509a154d9
anton lobanov <a.givertzman@icloud.com>
2023-01-09 21:04:58 +0300

    Chart

---
+++ lib/src/charts/crane_load_chart/crane_load_point_painter.dart
@@ -0,0 +1,134 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:crane_monitoring_app/domain/core/log/log.dart';
+import 'package:flutter/material.dart';
+
+///
+class CraneLoadPointPainter extends CustomPainter {
+  static const _debug = false;
+  final Map<int, String> _xAxis;
+  final Map<int, String> _yAxis;
+  final bool _showGrrid;
+  final List<Offset> _points;
+  final List<Color> _colors;
+  final int code;
+  final Size size;
+  final Color axisColor;
+  final Color _backgroundColor;
+  final double _pointSize;
+  ///
+  CraneLoadPointPainter({
+    required Map<int, String> xAxis,
+    required Map<int, String> yAxis,
+    required bool showGrid,
+    required List<Offset> points,
+    required List<Color> colors,
+    required this.size,
+    required this.axisColor,
+    required Color backgroundColor,
+    double pointSize = 1.0,
+  }) :
+    _xAxis = xAxis,
+    _yAxis = yAxis,
+    _showGrrid = showGrid,
+    _points = points,
+    _colors = colors,
+    _backgroundColor = backgroundColor,
+    _pointSize = pointSize,
+    code = Random().nextInt(1000),
+    super();
+  ///
+  @override
+  void paint(Canvas canvas, Size size) {
+    log(_debug, '[$CraneLoadPointPainter.paint]');
+    log(_debug, '[$CraneLoadPointPainter.paint] xAxis: ', _xAxis);
+    log(_debug, '[$CraneLoadPointPainter.paint] yAxis: ', _yAxis);
+    _drawBackgroundRect(canvas, size);
+    _drawXaxis(canvas, size);
+    _drawYaxis(canvas, size);
+    _drawPoints(canvas, size, _pointSize);
+  }
+  ///
+  void _drawBackgroundRect(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = _backgroundColor;
+    canvas.drawRect(
+      Rect.fromPoints(Offset.zero, Offset(size.width, size.height)), 
+      paint,
+    );
+  }
+  ///
+  void _drawXaxis(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = axisColor;
+    for (final xItem in _xAxis.entries.skip(1)) {
+      final textPainter = TextPainter(
+        text: TextSpan(
+          text: xItem.value,
+          style: TextStyle(color: axisColor),
+        ),
+        textDirection: TextDirection.ltr,
+      );
+      textPainter.layout(
+        maxWidth: size.width,
+      );
+      textPainter.paint(canvas, Offset(xItem.key.toDouble() - textPainter.size.width / 2, size.height * 0.97));
+      if (_showGrrid) {
+        canvas.drawLine(
+          Offset(xItem.key.toDouble(), 0), 
+          Offset(xItem.key.toDouble(), size.height * 0.965), 
+          paint,
+        );
+      }
+    }
+  }
+  ///
+  void _drawYaxis(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = axisColor;
+    for (final yItem in _yAxis.entries.skip(1)) {
+      final textPainter = TextPainter(
+        text: TextSpan(
+          text: yItem.value,
+          style: TextStyle(color: axisColor),
+        ),
+        textDirection: TextDirection.ltr,
+      );
+      textPainter.layout(
+        maxWidth: size.height,
+      );
+      textPainter.paint(canvas, Offset(0, size.height - yItem.key.toDouble() - textPainter.size.height * 0.5));
+      if (_showGrrid) {
+        canvas.drawLine(
+          Offset(size.width * 0.04, size.height - yItem.key.toDouble()),
+          Offset(size.width, size.height - yItem.key.toDouble()),
+          paint,
+        );
+      }
+    }
+  }
+  ///
+  void _drawPoints(Canvas canvas, Size size, double pointSize) {
+    Paint paint = Paint();
+      // ..style = PaintingStyle.fill;
+    for (int i = 0; i < _points.length; i++) {
+      paint = Paint()
+        ..color = _colors[i]
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth = pointSize;
+      canvas.drawPoints(
+        PointMode.points, 
+        [_points[i]], 
+        paint,
+      );
+    }
+  }
+  ///
+  @override
+  bool shouldRepaint(covariant CraneLoadPointPainter oldDelegate) {
+    // return true;
+    return (oldDelegate.code != code) || (oldDelegate.size != size);
+    // throw UnimplementedError();
+  }
+}
---
+++ lib/src/indicators/status_indicators/sps_icon_indicator.dart
@@ -0,0 +1,133 @@
+import 'package:crane_monitoring_app/domain/core/entities/ds_data_point.dart';
+import 'package:crane_monitoring_app/domain/core/entities/ds_status.dart';
+import 'package:crane_monitoring_app/domain/core/log/log.dart';
+import 'package:crane_monitoring_app/presentation/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+///
+/// Single Point status indicator.
+/// Simple icon, indicates state of streamed boolean.
+/// [DsDataPoint<bool>] value position
+/// by [trueIcon] & [falseIcon] passed as parameters
+class SpsIconIndicator extends StatefulWidget {
+  final Stream<DsDataPoint<bool>>? _stream;
+  final Icon? _trueIcon;
+  final Icon? _falseIcon;
+  ///
+  const SpsIconIndicator({
+      Key? key,
+      Stream<DsDataPoint<bool>>? stream,
+      Icon? trueIcon,
+      Icon? falseIcon,
+  }) : 
+    _stream = stream,
+    _trueIcon = trueIcon,
+    _falseIcon = falseIcon,
+    super(key: key);
+  ///
+  @override
+  // ignore: no_logic_in_create_state
+  State<SpsIconIndicator> createState() => _SpsIconIndicatorState(
+    stream: _stream,
+    trueIcon: _trueIcon,
+    falseIcon: _falseIcon,
+  );
+}
+
+///
+class _SpsIconIndicatorState extends State<SpsIconIndicator> {
+  static const _debug = true;
+  final Stream<DsDataPoint<bool>>? _stream;
+  final Icon? _trueIcon;
+  final Icon? _falseIcon;
+  late StateColors _stateColors;
+  ///
+  _SpsIconIndicatorState({
+      required Stream<DsDataPoint<bool>>? stream,
+      Icon? trueIcon,
+      Icon? falseIcon,
+  }) :
+    _stream = stream,
+    _trueIcon = trueIcon,
+    _falseIcon = falseIcon,
+    super();  
+  ///
+  @override
+  Widget build(BuildContext context) {
+    _stateColors = Theme.of(context).stateColors;
+    return StreamBuilder<DsDataPoint<bool>>(
+      // initialData: DsDataPoint<bool>(
+      //   type: DsDataType.bool, path: '', name: '', value: false, status: DsStatus.obsolete(), timestamp: '',
+      // ),
+      stream: _stream,
+      builder: (context, snapshot) {
+        return _buildIcon(
+          context,
+          snapshot,
+          _trueIcon,
+          _falseIcon,
+        );
+      },
+    );
+  }
+  ///
+  Widget _buildIcon(
+    BuildContext context, 
+    AsyncSnapshot<DsDataPoint<bool>> snapshot,
+    Icon? trueIcon, 
+    Icon? falseIcon,
+  ) {
+    Color color = _stateColors.invalid; //Theme.of(context).backgroundColor;
+    if (snapshot.hasError) {
+      color = Theme.of(context).errorColor;
+    } else if (snapshot.hasData) {
+      final point = snapshot.data;
+      if (point != null) {
+        color = _buildColor(point, color);
+        if (point.value) {
+          return trueIcon ?? _trueIconDefault(context, color);
+        } else {
+          return falseIcon ?? _falseIconDefault(context, color);
+        }
+      } else {
+        log(_debug, '[$_SpsIconIndicatorState._build] snapshot.connectionState: ', snapshot.connectionState);
+      }
+    } else {
+      log(_debug, '[$_SpsIconIndicatorState._build] snapshot.connectionState: ', snapshot.connectionState);
+    }
+    return falseIcon ?? _falseIconDefault(context, color);
+  }
+  ///
+  /// расчитывает текущий цвет в зависимости от point.status и point.value
+  Color _buildColor(DsDataPoint<bool> point, Color color) {
+    Color clr = color;
+    if (point.status == DsStatus.ok) {
+      clr = point.value
+        ? _stateColors.on
+        : _stateColors.off;
+    }
+    if (point.status == DsStatus.obsolete) {
+      clr = _stateColors.obsolete;
+    }
+    if (point.status == DsStatus.invalid) {
+      clr = _stateColors.invalid;
+    }
+    if (point.status == DsStatus.timeInvalid) {
+      clr = _stateColors.timeInvalid;
+    }
+    return clr;
+  }
+  ///
+  Icon _falseIconDefault(BuildContext context, Color? color) {
+    return Icon(
+      Icons.circle, 
+      color: color, //Theme.of(context).backgroundColor,
+    );
+  }
+  Icon _trueIconDefault(BuildContext context, Color? color) {
+    return Icon(
+      Icons.circle,
+      color: color, //Theme.of(context).colorScheme.primary,
+    );
+  }
+}
