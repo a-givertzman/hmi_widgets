755ea8bc81bda49cd68011c5cadbaf9aa3993ae2
Minyewoo <const.trushov@gmail.com>
2023-01-11 13:27:03 +0300

    dialogs

---
+++ lib/src/dialogs/complete_dialog.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void showCompleteDialog(BuildContext context, {Widget? title, Widget? content}) {
+  showDialog<bool>(
+    context: context, 
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: title,
+        content: content,
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true), 
+            child: const Text('Ok'),
+          ),
+        ],
+      );
+    },
+  );
+}
