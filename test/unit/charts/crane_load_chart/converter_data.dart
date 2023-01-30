3572630695683c805b3e3e975e6bffcc21749b4e
Minyewoo <const.trushov@gmail.com>
2023-01-30 14:18:07 +0300

    Made test data for SwlConverter

---
+++ test/unit/charts/crane_load_chart/converter_data.dart
@@ -0,0 +1,74 @@
+
+import 'package:flutter/material.dart';
+///
+///              SWL COLORING (FAKE VALUES)
+/// 
+///     0.0   4631.56   11,387.42  18,555.321  20,000.0
+/// 
+///      |-------|---------|-----------|----------|
+/// 
+///      | GREEN | YELLOW  |   ORANGE  |    RED   |
+const limitSet = [4631.56, 11387.42, 18555.321];
+const colorSet = [Colors.green, Colors.yellow, Colors.orange, Colors.red];
+const converterData = [
+  {
+    'point': Offset(0, 0),
+    'swl': [0.0, 20000.0],
+    'color': [Colors.green, Colors.red],
+  },
+  {
+    'point': Offset(0, 1),
+    'swl': [1234.12, 18556.99],
+    'color': [Colors.green, Colors.red],
+  },
+  {
+    'point': Offset(1, 0),
+    'swl': [4631.56, 18555.322],
+    'color': [Colors.green, Colors.red],
+  },
+  {
+    'point': Offset(234.85, 154.4),
+    'swl': [4631.57, 18555.321],
+    'color': [Colors.yellow, Colors.orange],
+  },
+  {
+    'point': Offset(289.85, 305.4),
+    'swl': [5678.456, 15678.432],
+    'color': [Colors.yellow, Colors.orange],
+  },
+  {
+    'point': Offset(456.543095, 142.34878394),
+    'swl': [11387.42, 11387.43],
+    'color': [Colors.yellow, Colors.orange],
+  },
+  {
+    'point': Offset(498.3434905, 346.345),
+    'swl': [11387.43, 11387.42],
+    'color': [Colors.orange, Colors.yellow],
+  },
+  {
+    'point': Offset(123.3434905, 456.345),
+    'swl': [15678.432, 5678.456],
+    'color': [Colors.orange, Colors.yellow],
+  },
+  {
+    'point': Offset(456.3578798, 123.3489),
+    'swl': [18555.321, 4631.57],
+    'color': [Colors.orange, Colors.yellow],
+  },
+  {
+    'point': Offset(275.094045, 296.897599),
+    'swl': [18555.322, 4631.56],
+    'color': [Colors.red, Colors.green],
+  },
+  {
+    'point': Offset(355.094045, 253.897599),
+    'swl': [18556.99, 1234.12],
+    'color': [ Colors.red, Colors.green],
+  },
+  {
+    'point': Offset(179, 197),
+    'swl': [20000.0, 0.0],
+    'color': [Colors.red, Colors.green],
+  },
+];
