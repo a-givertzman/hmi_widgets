27dff797bc9c97fe98d20fde3c1456d05a0b32b2
anton lobanov <a.givertzman@icloud.com>
2023-03-03 19:55:29 +0300

    CircularProgressIndicatorStreamed fixed

--- lib/src/indicators/status_indicators/circular_progress_idicator_streamed.dart
+++ lib/src/indicators/status_indicators/circular_progress_idicator_streamed.dart
@@ -2,26 +2,26 @@ import 'package:flutter/cupertino.dart';
 import 'package:hmi_core/hmi_core.dart';
 
 class CircularProgressIndicatorStreamed extends StatelessWidget {
-  final Stream<int>? _stream;
+  final Stream<OperationState>? _stream;
   ///
   const CircularProgressIndicatorStreamed({
     super.key,
-    required Stream<int>? stream,
+    required Stream<OperationState>? stream,
   }) :
     _stream = stream;
   //
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<int>(
+    return StreamBuilder<OperationState>(
       stream: _stream,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final data = snapshot.data;
           if (data != null) {
-            if (data == stateIsLoading) {
+            if (data == OperationState.inProgress) {
               return const FittedBox(child: CupertinoActivityIndicator());
             }
-            if (data == stateIsLoaded) {
+            if (data == OperationState.success || data == OperationState.failure) {
               return const Icon(null);
               // return Icon(Icons.account_tree_sharp, color: Theme.of(context).stateColors.on);
             }
